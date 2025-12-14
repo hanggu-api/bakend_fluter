aba01eb50823036972ee8f46d908ea2aa86d293c
Dvik-colab <atendimentodvick@gmail.com>
2025-12-13 22:29:01 -0300

    fix(vercel): remover import de tipos '@vercel/node' do handler

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: any, res: any) {
   (app as any)(req, res);
 }
